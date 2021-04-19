ee96a69f670bb2b736607a0147c1544268ab24e0
Mars Gotta <you@example.com>
2021-04-19 10:37:37 +0200

    feat: added react form

--- owl-book/src/app/owl-login/components/reactive-form/reactive-form.component.ts
+++ owl-book/src/app/owl-login/components/reactive-form/reactive-form.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -7,7 +13,14 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./reactive-form.component.css'],
 })
 export class ReactiveFormComponent implements OnInit {
-  constructor(public fb: FormBuilder) {}
+  constructor(public fb: FormBuilder, private cd: ChangeDetectorRef) {}
+  countries: Array<string> = [
+    'España',
+    'Italia',
+    'Francia',
+    'Alemania',
+    'Mexico',
+  ];
 
   registrationForm = this.fb.group({
     file: [null],
@@ -34,7 +47,7 @@ export class ReactiveFormComponent implements OnInit {
     address: this.fb.group({
       street: ['', [Validators.required]],
       city: ['', [Validators.required]],
-      cityName: ['', [Validators.required]],
+      country: ['', [Validators.required]],
     }),
     gender: ['', [Validators.required]],
     passwordValidation: this.fb.group({
@@ -46,6 +59,54 @@ export class ReactiveFormComponent implements OnInit {
 
   get myForm() {
     return this.registrationForm.controls;
+  }
+
+  get addDynamicElement() {
+    return this.registrationForm.get('addDynamicElement') as FormArray;
+  }
+
+  changeCountry(e: any): void {
+    // Opción para setear valores en el formulario desde el controlador.
+    //this.registrationForm.get('address.country')?.setValue(e.target.value);
+  }
+
+  addSkill(): void {
+    this.addDynamicElement.push(this.fb.control(''));
+    console.log(this.addDynamicElement);
+  }
+
+  @ViewChild('fileInput') el: ElementRef = new ElementRef('');
+  editFile: boolean = true;
+  removeUpdload: boolean = false;
+  imageUrl: any = '';
+
+  updloadFile(e: any): void {
+    let reader = new FileReader(); // HTML5 FileReader API
+    let file = e.target.files[0];
+    if (e.target?.files && e.target?.files[0]) {
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+        this.registrationForm.patchValue({
+          file: reader.result,
+        });
+        this.editFile = false;
+        this.removeUpdload = true;
+      };
+      this.cd.markForCheck();
+    }
+  }
+
+  removeUpdloadFile(): void {
+    let newFileList = Array.from(this.el.nativeElement.files);
+    if (newFileList && newFileList[0]) {
+      this.imageUrl = '';
+      this.removeUpdload = false;
+      this.editFile = true;
+      this.registrationForm.patchValue({
+        file: [null],
+      });
+    }
   }
 
   ngOnInit(): void {}
